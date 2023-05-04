9257b19bd4bf2dc278d68ab577b2a7869b16061a
Lokesh Pereiro <lpdraft001@gmail.com>
2023-05-04 18:05:30 +0200

    adding prods

---
+++ CartApp/src/components/CatalogView.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { getProducts } from "../services/productService";
+import { ProductCardView } from "./ProductCardView";
+
+export const CatalogView = ({ handler }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(getProducts());
+  }, []);
+
+  return (
+    <>
+      <div className="row">
+        {products.map((prod) => (
+          <div className="col-4 my-2" key={prod.id}>
+            <ProductCardView {...prod} handler={handler} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
---
+++ CartApp/src/components/ProductCardView.jsx
@@ -0,0 +1,25 @@
+import { products } from "../data/products";
+
+export const ProductCardView = ({ handler, id, name, description, price }) => {
+  const onAddProduct = (product) => {
+    // console.log(product);
+    handler(product);
+  };
+  return (
+    <>
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">$ {price}</p>
+          <button
+            onClick={() => onAddProduct({ id, name, description, price })}
+            className="btn btn-primary"
+          >
+            Agregar
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
