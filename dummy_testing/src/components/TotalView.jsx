8f94f1fe374dfb2b0904309207009fb3aaa2cae8
LokeshPereiro <126457016+LokeshPereiro@users.noreply.github.com>
2023-08-14 09:29:35 +0200

    has already been set in the main branch

--- dummy_testing/src/components/TotalView.jsx
+++
@@ -1,8 +0,0 @@
-import PropTypes from "prop-types";
-
-export const TotalView = ({ total }) => {
-  return <div className="badge bg-success">{total} €</div>;
-};
-TotalView.propTypes = {
-  total: PropTypes.number,
-};
